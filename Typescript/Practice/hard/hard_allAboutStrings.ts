6961ea8eae9796af5ef11035a39f2701f3e74c63
joshuacao-verent <107446460+joshuacao-verent@users.noreply.github.com>
2022-08-13 00:42:11 +1000

    all about string

---
+++ Typescript/Practice/hard/hard_allAboutStrings.ts
@@ -0,0 +1,59 @@
+#!/usr/bin/env npx ts-node
+
+
+// get the length of the str
+function getLength(str: string): number {
+    return str.length;
+};
+
+// get the first character of the str
+function firstChar(str: string): string {
+    return str[0];
+};
+
+// get the last character of the str
+function lastChar(str: string): string {
+    return str.slice(-1);
+};
+
+// get middle char(s)
+function middleChar(str: string): string {
+    let str_length: number = getLength(str);
+    if (str_length % 2 === 0) {
+        return str.slice(str_length / 2 - 1, str_length / 2 + 1);
+    } else {
+        return str[Math.floor(str_length / 2)]
+    };
+};
+
+// get index of the second character
+function indexSecond(str: string): string {
+    let second: string = str[1];
+    for (let idx in Array.from(str)) {
+        if (Number(idx) > 1 && str[idx] === second) {
+            return `@ index ${idx}`
+        };
+    };
+    return "not found";
+};
+
+// main function
+function allAboutStrings(str: string): Array<number | string> | string {
+    if (str.length >= 3) {
+        return [
+            getLength(str),
+            firstChar(str),
+            lastChar(str),
+            middleChar(str),
+            indexSecond(str)
+        ];
+    } else {
+        return "Please input valid string (e.g. string with at least 3 characters.)";
+    };
+};
+
+
+console.log(allAboutStrings("LASA"));
+console.log(allAboutStrings("Computer"));
+console.log(allAboutStrings("Science"));
+console.log(allAboutStrings("ab"));
